0a1cf3bed965b3e5235d0be23649a802521eaa95
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-03-12 21:10:46 +0600

    search with enter button

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('keypress', function (event) {
+  if (event.key == 'Enter') {
+    searchButton.click();
+  }
+})
 document.getElementById("search-button").addEventListener("click", function () {
-    const input = document.getElementById("search-input");
-    const inputText = input.value;
-    if (inputText == '') {
-        let error = document.getElementById('error')
-        error.innerText="Opps! Please Enter a Valid Keyword"
-    }
-    else {
-        const error = document.getElementById('error')
-        error.style.display = 'none';
-        console.log(inputText);
-        input.value = "";
-        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`;
-        fetch(url)
-          .then((res) => res.json())
-          .then((data) => displayFood(data.meals));
- }
+  const input = document.getElementById("search-input");
+  const inputText = input.value;
+  if (!inputText) {
+    const container = document.getElementById("container");
+    container.innerHTML = "";
+    let error = document.getElementById("error1");
+    error.innerText = "Please Enter something!";
+  } else {
+    const error = document.getElementById("error1");
+    error.style.display = "none";
+    input.value = "";
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayFood(data.meals));
+  }
 });
 const displayFood = (foods) => {
-  const container = document.getElementById("container");
-  container.innerHTML = "";
+  if (foods == null || undefined) {
+    const container = document.getElementById("container");
+    container.innerHTML = "";
+    let error = document.getElementById("error2");
+    error.innerText = "Opps! Results not found";
+  } else {
+    let error = document.getElementById("error2");
+    error.innerText = "";
+    const container = document.getElementById("container");
+    container.innerHTML = "";
 
-  foods.forEach((food) => {
-    // console.log(food)
-    const div = document.createElement("div");
-    div.classList.add("col");
+    foods?.forEach((food) => {
+      // console.log(food)
+      const div = document.createElement("div");
+      div.classList.add("col");
 
-    div.innerHTML = `
+      div.innerHTML = `
                 <div onclick="displayFoodDetail(${food.idMeal})" class="card">
                 <img src="${food.strMealThumb}" class="card-img-top" alt="..." />
                 <div class="card-body">
@@ -35,8 +50,9 @@ const displayFood = (foods) => {
               </div>
         
                 `;
-    container.appendChild(div);
-  });
+      container.appendChild(div);
+    });
+  }
 };
 function displayFoodDetail(mealId) {
   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
